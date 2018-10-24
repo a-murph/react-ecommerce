75b79756964db463aa70d583324f4d74e3fad089
Andrew Murphy <ajmurph544@gmail.com>
2018-10-23 21:46:54 -0400

    added product page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Product from "./pages/Product";
 import "./css/index.css"
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
 		<div>
 			<Route exact path="/" component={Home} />
 			<Route path="/search" component={Search} />
+			<Route path="/product" component={Product} />
 		</div>
 	</Router>
 );
---
+++ client/src/pages/Product.js
@@ -0,0 +1,224 @@
+import React, {Component} from "react";
+
+class Product extends Component {
+	render() {
+		return (
+			<div>
+				<nav class="menu-bar">
+					<div class="nav-location-button"></div>
+					<div class="nav-home-logo"></div>
+					<div class="nav-search-bar">
+						<div class="nav-search-filter-button"></div>
+						<div class="nav-search-text-input"></div>
+						<div class="nav-search-submit-button"></div>
+					</div>
+					<div class="nav-language-dropdown"> </div>
+					<div class="nav-div-bar"></div>
+					<div class="nav-account-dropdown"> </div>
+					<div class="nav-orders-dropdown"> </div>
+					<div class="nav-cart-button"> </div>
+				</nav>
+
+				<main>
+					<div class="product-page-top-bar">
+						<div class="top-bar-search-result"><span>Clothing/Big & Tall/Mens Big & Tall T-Shirts & Tank Tops/Mens Big & Tall T-Shirts & Tank Tops</span></div>
+						<div class="top-bar-social-media-share">
+							<a class="social-media-share-link" href="#"><img class="social-media-share-img" src="#"/></a>
+							<a class="social-media-share-link" href="#"><img class="social-media-share-img" src="#"/></a>
+							<a class="social-media-share-link" href="#"><img class="social-media-share-img" src="#"/></a>
+						</div>
+					</div>
+
+					<div class="product-main">
+						<div class="product-main-display">
+							<img class="product-main-display-img" src="img/product1.jpeg"/>
+						</div>
+						<div class="product-main-info">
+							<div class="main-info-title">
+								<span>Men's Platinum Eversoft Short Sleeve Crew T Shirt</span>
+								<a href="#"><span>Fruit of the Loom</span></a>
+								<div>
+									<img src="#"/>
+									<a href="#"><span>542 Customer Reviews</span></a>
+								</div>
+							</div>
+
+							<div class="main-info-price">
+									<div><span>List Price:</span> <span>$42.50</span></div>
+									<div><span>Price:</span> <span>$37.19</span> <span>& FREE Shipping</span></div>
+									<div><span>You Save:</span> <span>$5.31 (12%)</span></div>
+							</div>
+
+
+							<div class="main-info-sizing-color">
+									<div><span>Size:</span> <span>Small</span></div>
+									<div><span>Color:</span> <span>Black</span></div>
+							</div>
+
+
+							<div class="main-info-quantity-add-to-card">
+									<div><span>Quantity:</span> <input type="text"/></div>
+									<button type="button">Add to Cart</button>
+							</div>
+
+							<div class="main-info-add-to-list">
+									<a href="#"><img src="#"/><span>Add to List</span></a>
+									<a href="#"><img src="#"/><span>Add to Registry</span></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="product-description">
+						<div class="product-description-text">
+							<span>About This Item</span>
+							<p>We aim to show you accurate product information. Manufacturers, suppliers and others provide what you see here, and we have not verified it. </p>
+							<p>The Fruit of the Loom Platinum EverSoft Men's Crew t-shirt is America's #1 selling t-shirt brand. These t-shirts are made with EverSoft ring spun cotton and now feature Dual Defense technology providing wicking and odor protection. From mowing the yard to hanging out with friends, we know your t-shirt endures a lot, so we incorporated a ribbed collar that won't lose its shape, as well as double-needle stitching on the neck and shoulders to give you more durability. This t-shirt is tag-free and available in a variety of colors to choose from. </p>
+							<span>Fruit of the Loom Men's Platinum Eversoft Short Sleeve Crew T Shirt, up to Size 4XL:</span>
+							<ul>
+								<li>Mid-weight fabric that feels substantial, yet still soft</li>
+								<li>EverSoft ring spun cotton provides premium softness wash after wash</li>
+								<li>Dual Defense Technology for wicking & odor protection</li>
+								<li>Fits generously through the body</li>
+								<li>Ribbed collar holds its shape</li>
+								<li>Shoulder-to-shoulder neck tape for comfort and durability</li>
+								<li>Double-needle stitching on the neck and shoulders provide superior durability</li>
+								<li>Sizes S-4XL</li>
+								<li>Available in a variety of stylish colors</li>
+							</ul>
+						</div>
+						<img src="#" class="product-description-img"/>
+					</div>
+
+					<div class="recommended-products">
+						<span>Customers Also Bought These Products</span>
+						<div class="recommended-products-card-section">
+							<a class="recommended-products-card-anchor" href="#"><div class="recommended-products-card">
+								<img class="card-img" src="#"/>
+								<div class="card-text">
+									<span>Men's Platinum EverSoft Muscle</span>
+									<span>Fruit of the Loom</span>
+									<div><img class="" src="#"/><span>288</span></div>
+									<span>$6.47</span>
+								</div>
+							</div></a>
+
+							<a class="recommended-products-card-anchor" href="#"><div class="recommended-products-card">
+								<img class="card-img" src="#"/>
+								<div class="card-text">
+									<span>Men's Platinum EverSoft Muscle</span>
+									<span>Fruit of the Loom</span>
+									<div><img class="" src="#"/><span>288</span></div>
+									<span>$6.47</span>
+								</div>
+							</div></a>
+
+							<a class="recommended-products-card-anchor" href="#"><div class="recommended-products-card">
+								<img class="card-img" src="#"/>
+								<div class="card-text">
+									<span>Men's Platinum EverSoft Muscle</span>
+									<span>Fruit of the Loom</span>
+									<div><img class="" src="#"/><span>288</span></div>
+									<span>$6.47</span>
+								</div>
+							</div></a>
+
+							<a class="recommended-products-card-anchor" href="#"><div class="recommended-products-card">
+								<img class="card-img" src="#"/>
+								<div class="card-text">
+									<span>Men's Platinum EverSoft Muscle</span>
+									<span>Fruit of the Loom</span>
+									<div><img class="" src="#"/><span>288</span></div>
+									<span>$6.47</span>
+								</div>
+							</div></a>
+
+							<a class="recommended-products-card-anchor" href="#"><div class="recommended-products-card">
+								<img class="card-img" src="#"/>
+								<div class="card-text">
+									<span>Men's Platinum EverSoft Muscle</span>
+									<span>Fruit of the Loom</span>
+									<div><img class="" src="#"/><span>288</span></div>
+									<span>$6.47</span>
+								</div>
+							</div></a>
+
+							<a class="recommended-products-card-anchor" href="#"><div class="recommended-products-card">
+								<img class="card-img" src="#"/>
+								<div class="card-text">
+									<span>Men's Platinum EverSoft Muscle</span>
+									<span>Fruit of the Loom</span>
+									<div><img class="" src="#"/><span>288</span></div>
+									<span>$6.47</span>
+								</div>
+							</div></a>
+
+						</div>
+					</div>
+
+					<div class="customer-reviews">
+						<div class="customer-reviews-bar">
+							<div class="reviews-summary">
+								<span>289 Customer Reviews</span>
+								<div><img src="#" class="total-review-stars"/><span>3.8 out of 5 stars</span></div>
+								<div class="customer-reviews-breakdown-bars">
+									<a href="#"><span>5 Star</span><img src="#"/><span>49%</span></a>
+									<a href="#"><span>4 Star</span><img src="#"/><span>21%</span></a>
+									<a href="#"><span>3 Star</span><img src="#"/><span>7%</span></a>
+									<a href="#"><span>2 Star</span><img src="#"/><span>9%</span></a>
+									<a href="#"><span>1 Star</span><img src="#"/><span>14%</span></a>
+								</div>
+							</div>
+
+							<div class="write-a-review">
+								<span>Review this Product</span>
+								<span>Share your thoughts with other customers</span>
+								<button class="write-a-review-button" type="button">Write a Review</button>
+							</div>
+						</div>
+
+						<div class="reviews-section">
+							<div class="review">
+								<div class="review-item"><img src="#"/><span>Username</span></div>
+								<div class="review-item"><img src="#"/><span>Review Title</span></div>
+								<span class="review-item">February 3rd, 2018</span>
+								<div class="review-item"><span>Size: Medium</span><span>Color: White</span><span>Verified Purchase</span></div>
+								<div class="review-item"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id imperdiet purus. Duis vitae bibendum lorem, vel pharetra enim. Quisque tincidunt porttitor nisi, id convallis augue gravida id. Aliquam viverra id ante ut vehicula. Nullam porttitor eros libero, id maximus tortor scelerisque eu. Ut nisl tellus, pharetra non elit eget, tempus condimentum nulla. In quis diam id orci fermentum accumsan ut id orci. Duis sollicitudin pharetra tempor. Donec sed ligula blandit, sodales nunc id, tempus enim. Donec ultricies eu velit semper semper. Sed nec faucibus nibh. </p></div>
+								<span class="review-item">7 people found this helpful</span>
+								<div class="review-item review-buttons"> <a href="#"><img src="#"/><span>Helpful</span></a> <a href="#"><span>Comment</span></a> <a href="#"><span>Report Abuse</span></a></div>
+							</div>
+							<div class="review">
+								<div class="review-item"><img src="#"/><span>Username</span></div>
+								<div class="review-item"><img src="#"/><span>Review Title</span></div>
+								<span class="review-item">February 3rd, 2018</span>
+								<div class="review-item"><span>Size: Medium</span><span>Color: White</span><span>Verified Purchase</span></div>
+								<div class="review-item"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id imperdiet purus. Duis vitae bibendum lorem, vel pharetra enim. Quisque tincidunt porttitor nisi, id convallis augue gravida id. Aliquam viverra id ante ut vehicula. Nullam porttitor eros libero, id maximus tortor scelerisque eu. Ut nisl tellus, pharetra non elit eget, tempus condimentum nulla. In quis diam id orci fermentum accumsan ut id orci. Duis sollicitudin pharetra tempor. Donec sed ligula blandit, sodales nunc id, tempus enim. Donec ultricies eu velit semper semper. Sed nec faucibus nibh. </p></div>
+								<span class="review-item">7 people found this helpful</span>
+								<div class="review-item review-buttons"> <a href="#"><img src="#"/><span>Helpful</span></a> <a href="#"><span>Comment</span></a> <a href="#"><span>Report Abuse</span></a></div>
+							</div>
+							<div class="review">
+								<div class="review-item"><img src="#"/><span>Username</span></div>
+								<div class="review-item"><img src="#"/><span>Review Title</span></div>
+								<span class="review-item">February 3rd, 2018</span>
+								<div class="review-item"><span>Size: Medium</span><span>Color: White</span><span>Verified Purchase</span></div>
+								<div class="review-item"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id imperdiet purus. Duis vitae bibendum lorem, vel pharetra enim. Quisque tincidunt porttitor nisi, id convallis augue gravida id. Aliquam viverra id ante ut vehicula. Nullam porttitor eros libero, id maximus tortor scelerisque eu. Ut nisl tellus, pharetra non elit eget, tempus condimentum nulla. In quis diam id orci fermentum accumsan ut id orci. Duis sollicitudin pharetra tempor. Donec sed ligula blandit, sodales nunc id, tempus enim. Donec ultricies eu velit semper semper. Sed nec faucibus nibh. </p></div>
+								<span class="review-item">7 people found this helpful</span>
+								<div class="review-item review-buttons"> <a href="#"><img src="#"/><span>Helpful</span></a> <a href="#"><span>Comment</span></a> <a href="#"><span>Report Abuse</span></a></div>
+							</div>
+							<div class="review">
+								<div class="review-item"><img src="#"/><span>Username</span></div>
+								<div class="review-item"><img src="#"/><span>Review Title</span></div>
+								<span class="review-item">February 3rd, 2018</span>
+								<div class="review-item"><span>Size: Medium</span><span>Color: White</span><span>Verified Purchase</span></div>
+								<div class="review-item"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id imperdiet purus. Duis vitae bibendum lorem, vel pharetra enim. Quisque tincidunt porttitor nisi, id convallis augue gravida id. Aliquam viverra id ante ut vehicula. Nullam porttitor eros libero, id maximus tortor scelerisque eu. Ut nisl tellus, pharetra non elit eget, tempus condimentum nulla. In quis diam id orci fermentum accumsan ut id orci. Duis sollicitudin pharetra tempor. Donec sed ligula blandit, sodales nunc id, tempus enim. Donec ultricies eu velit semper semper. Sed nec faucibus nibh. </p></div>
+								<span class="review-item">7 people found this helpful</span>
+								<div class="review-item review-buttons"> <a href="#"><img src="#"/><span>Helpful</span></a> <a href="#"><span>Comment</span></a> <a href="#"><span>Report Abuse</span></a></div>
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+		);
+	}
+}
+
+export default Product;
