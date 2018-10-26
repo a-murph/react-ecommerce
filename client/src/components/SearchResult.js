24366124feef452bd776b0d3d1663b0c646b27d2
Andrew Murphy <ajmurph544@gmail.com>
2018-10-26 13:36:50 -0400

    changed product page links to Link elements

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,14 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const SearchResult = props => (
-	<a className="search-result-card" href={`/product/${props.product._id}`}>
+	<Link className="search-result-card" to={{ pathname: `/product/${props.product._id}`, state: { product: props.product} }}>
 		<img alt="" className="search-result-card-img" src={props.product.image}/>
 		<div className="search-result-card-text">
 			<span>{props.product.name}</span>
 			<div><span>${props.product.price.toFixed(2)}</span> <span>(insert img) free shipping tag</span></div>
 			<div><span>{props.product.rating} Stars </span><span>({props.product.reviews.length} Reviews)</span></div>
 		</div>
-	</a>
+	</Link>
 );
 
 export default SearchResult
