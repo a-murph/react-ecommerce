cbd20ec09baca15ef547e5600f2d63687e07775a
Andrew Murphy <ajmurph544@gmail.com>
2018-12-05 18:11:30 -0500

    change current selection

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -4,24 +4,44 @@ class Dropdown extends Component {
 	state = {
 		current: "",
 		previous: ""
-	}
+	};
 
 	showDropdown = () => {
 		document.getElementById("dropdown-options").classList.toggle("hidden");
-	}
+	};
+
+	changeSelection = event => {
+		event.persist();
+
+		let prev = document.getElementById("dropdown-selection").textContent;
+		let next = event.target.textContent;
+
+		this.setState({
+			current: next,
+			previous: prev
+		}, () => {
+			event.target.textContent = prev
+		});
+	};
+
+	componentDidMount() {
+		this.setState({
+			current: "All"
+		});
+	};
 
 	render() {
 		return (
 			<div className="dropdown">
 				<div id="dropdown-selection" onClick={this.showDropdown}>
-					All
+					{this.state.current}
 				</div>
 				<div id="dropdown-options" className="dropdown-content hidden">
-					<div>Electronics</div>
-					<div>Media</div>
-					<div>Clothing</div>
-					<div>Health & Beauty</div>
-					<div>Home</div>
+					<div onClick={this.changeSelection}>Electronics</div>
+					<div onClick={this.changeSelection}>Media</div>
+					<div onClick={this.changeSelection}>Clothing</div>
+					<div onClick={this.changeSelection}>Health & Beauty</div>
+					<div onClick={this.changeSelection}>Home</div>
 				</div>
 			</div>
 		);
