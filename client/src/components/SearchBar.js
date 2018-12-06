51ac404d696a2355176d46fb42ba6e6f48b743c3
Andrew Murphy <ajmurph544@gmail.com>
2018-12-05 19:03:47 -0500

    renamed Dropdown to Select and wired it up to SearchBar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Select from "./Select";
 
 class SearchBar extends Component {
 	state = {
@@ -12,7 +13,6 @@ class SearchBar extends Component {
 				searchTerm: event.target.value
 			});
 		} else if (event.target.name === "dropdown") {
-			console.log("in if")
 			this.setState({
 				category: event.target.value
 			});
@@ -27,7 +27,8 @@ class SearchBar extends Component {
 		if (this.props.page && this.props.page === "home")
 			return (
 				<div className="home-nav-search-bar">
-					<div className="home-nav-search-filter-button">
+
+					{/* <div className="home-nav-search-filter-button">
 						<select onChange={this.handleSearchInput} name="dropdown" className="nav-font-dark">
 							<option>All</option>
 							<option>Electronics</option>
@@ -36,7 +37,10 @@ class SearchBar extends Component {
 							<option>Home</option>
 							<option>Media</option>
 						</select>
-					</div>
+					</div> */}
+
+					<Select handleChange={this.handleSearchInput} options={["Electronics", "Clothing", "Home", "Health and Beauty", "Media"]} default="All" />
+
 					<input type="text" name="search-bar" className="home-nav-search-text-input" onChange={this.handleSearchInput} onKeyPress={event => { if (event.key && event.key === "Enter") this.handleSearch() }}></input>
 					<div className="home-nav-search-submit-button"><input type="submit" className="icon" value="" onClick={this.handleSearch}></input><i className="fas fa-search icon"></i></div>
 				</div>
@@ -44,16 +48,20 @@ class SearchBar extends Component {
 
 		return (
 			<div className="nav-search-bar">
-				<div className="nav-search-filter-button">
-					<select onChange={this.handleSearchInput} name="dropdown" className="nav-font-dark">
-						<option>All</option>
-						<option>Electronics</option>
-						<option>Clothing</option>
-						<option>Health and Beauty</option>
-						<option>Home</option>
-						<option>Media</option>
+
+				{/* <div className="dropdown">
+					<select onChange={this.handleSearchInput} name="dropdown" className="nav-font-dark drop-button">
+						<option className="drop-content">All</option>
+						<option className="drop-content">Electronics</option>
+						<option className="drop-content">Clothing</option>
+						<option className="drop-content">Health and Beauty</option>
+						<option className="drop-content">Home</option>
+						<option className="drop-content">Media</option>
 					</select>
-				</div>
+				</div> */}
+
+				<Select handleChange={this.handleSearchInput} options={["Electronics", "Clothing", "Home", "Health & Beauty", "Media"]} default="All" />
+
 				<input type="text" name="search-bar" className="nav-search-text-input" onChange={this.handleSearchInput} onKeyPress={event => { if (event.key && event.key === "Enter") this.handleSearch() }}></input>
 				<div className="nav-search-submit-button"><input type="submit" className="icon" value="" onClick={this.handleSearch}></input><i className="fas fa-search icon"></i></div>
 			</div>
---
+++ client/src/components/Select.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+
+class Select extends Component {
+	state = {
+		current: "",
+		previous: ""
+	};
+
+	showDropdown = event => {
+		event.target.nextSibling.classList.toggle("hidden");
+	};
+
+	changeSelection = event => {
+		event.persist();
+
+		let prev = event.target.parentElement.previousSibling.textContent;
+		let next = event.target.textContent;
+
+		this.setState({
+			current: next,
+			previous: prev
+		}, () => {
+			event.target.textContent = prev;
+			event.target.parentElement.classList.toggle("hidden");
+			this.props.handleChange({
+				target: {
+					name: "dropdown",
+					value: event.target.textContent
+				}
+			});
+		});
+	};
+
+	componentDidMount() {
+		this.setState({
+			current: this.props.default
+		});
+	};
+
+	render() {
+		return (
+			<div className="dropdown">
+				<div className="dropdown-selection" onClick={this.showDropdown}>
+					{this.state.current}
+				</div>
+				<div className="dropdown-content hidden">
+					{this.props.options.map((option, index) => {
+						return <div key={index} onClick={this.changeSelection}>{option}</div>
+					})}
+				</div>
+			</div>
+		);
+	};
+};
+
+export default Select;
