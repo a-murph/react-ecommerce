2f637af1dffce041cd1f4c347e4757857ec36eb1
Andrew Murphy <ajmurph544@gmail.com>
2018-12-10 17:48:03 -0500

    added key to image divs

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -8,7 +8,7 @@ class ImgCarousel extends Component {
 		return (
 			<Carousel wrapAround={true} slidesToShow={this.props.slideCount} heightMode="max">
 				{this.props.type === "image" ? (this.props.items.map((image, index) => {
-					return <div className="img-container"><img key={index} alt="" className="product-main-display-img" src={image}/></div>
+					return <div key={index} className="img-container"><img alt="" className="product-main-display-img" src={image}/></div>
 				})) : this.props.type === "item" ? (
 					this.props.items.map((item, index) => {
 						return <RecommendedItem click={this.props.onClick} product={item} key={index} />
