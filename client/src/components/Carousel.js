eaf40c3b0c1d00fa3c8a45fdbfddd5d567c368bd
Andrew Murphy <ajmurph544@gmail.com>
2018-12-14 18:05:46 -0500

    changed from class component to functional

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,21 +1,17 @@
-import React, {Component} from "react";
+import React from "react";
 import Carousel from "nuka-carousel";
 import RecommendedItem from "./RecommendedItem";
 
-class ImgCarousel extends Component {
-	render() {
-		return (
-			<Carousel wrapAround={true} slidesToShow={this.props.slideCount} heightMode="max">
-				{this.props.type === "image" ? (this.props.items.map((image, index) => {
-					return <div key={index} className="img-container"><img alt="product" className="product-main-display-img" src={image}/></div>
-				})) : this.props.type === "item" ? (
-					this.props.items.map((item, index) => {
-						return <RecommendedItem click={this.props.onClick} product={item} key={index} />
-					})
-				) : ""}
-			</Carousel>
-		);
-	}
-};
+const ImgCarousel = props => (
+	<Carousel wrapAround={true} slidesToShow={props.slideCount} heightMode="max">
+		{props.type === "image" ? (props.items.map((image, index) => {
+			return <div key={index} className="img-container"><img alt="product" className="product-main-display-img" src={image}/></div>
+		})) : props.type === "item" ? (
+			props.items.map((item, index) => {
+				return <RecommendedItem click={props.onClick} product={item} key={index} />
+			})
+		) : ""}
+	</Carousel>
+);
 
 export default ImgCarousel;
